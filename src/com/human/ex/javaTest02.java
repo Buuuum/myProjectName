7ee85df9bbdb8c673641145eb8ec0cc90bbad3b5
admin <admin@DESKTOP-APDSTRB>
2022-03-03 15:29:08 +0900

    4 commit

--- src/com/human/ex/javaTest02.java
+++ src/com/human/ex/javaTest02.java
@@ -4,50 +4,52 @@ package com.human.ex;
 public class javaTest02 {
 	public static void main(String[] args) {
 
-		//		for(int i=1; i<6; i++) {
-		//		System.out.println("*****");
-		//	}
+
+	
+//		for(int i=1; i<6; i++) {
+//			System.out.println("*****");
+//		}
 
 
-		//	for(int i=1; i<6; i++) {
-		//		for(int j=1; j<i+1; j++) {
-		//			System.out.print("*");
-		//		}
-		//		System.out.println();
-		//	}
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<i+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 
 
 
-		//	for(int i=1; i<6; i++) {
-		//		for(int j=5-i; j>0; j--) {
-		//			System.out.print(" ");
-		//		}
-		//		for(int k=1; k<=i; k++) {
-		//			System.out.print("*");
-		//		}
-		//		System.out.println();
-		//	}
+//		for(int i=1; i<6; i++) {
+//			for(int j=5-i; j>0; j--) {
+//				System.out.print(" ");
+//			}
+//			for(int k=1; k<=i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 
 
-		//	System.out.println("*****************");
-		//	System.out.println("*     성적표      *");
-		//	System.out.println("*****************");
-		//	System.out.println("* 국어   50      *");
-		//	System.out.println("*****************");
-		//	System.out.println("* 영어   50      *");
-		//	System.out.println("*****************");
-		//	System.out.println("* 수어   50      *");
-		//	System.out.println("*****************");
-		//	System.out.println("* 컴퓨터학과  홍길동*");
-		//	System.out.println("*****************");
+//		System.out.println("*****************");
+//		System.out.println("*     성적표      *");
+//		System.out.println("*****************");
+//		System.out.println("* 국어   50      *");
+//		System.out.println("*****************");
+//		System.out.println("* 영어   50      *");
+//		System.out.println("*****************");
+//		System.out.println("* 수어   50      *");
+//		System.out.println("*****************");
+//		System.out.println("* 컴퓨터학과  홍길동*");
+//		System.out.println("*****************");
 
 
-		System.out.println("일\t월\t화\t수\t목\t금\t토");
-		System.out.println("1\t2\t3\t4\t5\t6\t7");
-		System.out.println("8\t9\t10\t11\t12\t13\t14");
-		System.out.println("15\t16\t17\t18\t19\t20\t21");
-		System.out.println("22\t23\t24\t25\t26\t27\t28");
-		System.out.println("29\t30\t31");
+//		System.out.println("일\t월\t화\t수\t목\t금\t토");
+//		System.out.println("1\t2\t3\t4\t5\t6\t7");
+//		System.out.println("8\t9\t10\t11\t12\t13\t14");
+//		System.out.println("15\t16\t17\t18\t19\t20\t21");
+//		System.out.println("22\t23\t24\t25\t26\t27\t28");
+//		System.out.println("29\t30\t31");
 
 
 	}

c733fbfa7faf454870b7d993455fc8e1b45a888e
kyb <kyb@218.38.137.27>
2022-03-04 12:27:22 +0900

    commit

---
+++ src/com/human/ex/JavaTest03.java
@@ -0,0 +1,43 @@
+package com.human.ex;
+
+public class JavaTest03 {
+
+	public static void main(String[] args) {
+		
+//		System.out.println(3.14F);
+//		System.out.println(3.14f);
+//		System.out.println(3f);
+//		System.out.println(3.f);
+//		System.out.println(.14f);
+//		System.out.println();
+//		
+//		System.out.println(3.14);
+//		System.out.println(3.);
+//		System.out.println(.14);
+//		System.out.println(3D);
+//		System.out.println(.14d);
+//		System.out.println(3.14D);
+//		System.out.println(3.14e-4);
+//		
+//		System.out.println(Math.PI);
+		
+		
+		
+//		String name = "홍길동";
+//		int age = 29;
+//		int height = 179;
+//		
+//		System.out.println("이름 : " + name);
+//		System.out.println("나이 : " + age);
+//		System.out.println("키 : " + height);
+		
+		
+		
+		int a = 5;
+		int b = 10;
+		
+		a += 3;
+		
+		System.out.println(a+b);
+	}
+}

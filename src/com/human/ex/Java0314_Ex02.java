c4b5ce82640274ea967da7b25ba82f8891b9bc97
admin <admin@DESKTOP-APDSTRB>
2022-03-14 15:26:07 +0900

    6 commit

--- src/com/human/ex/Java0314_Ex02.java
+++ src/com/human/ex/Java0314_Ex02.java
@@ -22,54 +22,54 @@ public class Java0314_Ex02 {
 		
 		
 //		문제2)
-//		Scanner sc = new Scanner(System.in);
-//		System.out.print("a : ");
-//		int a = Integer.parseInt(sc.nextLine());
-//		System.out.print("b : ");
-//		int b = Integer.parseInt(sc.nextLine());
-//		System.out.print("c : ");
-//		int c = Integer.parseInt(sc.nextLine());
-//		int d = 0;
-//		
-//		if(a<b) {
-//			if(b<c) {
-////				a<b<c
-//			}else if(a<c) {
-////				a<c<b
-//				d = b;
-//				b = c;
-//				c = d;
-//			}
-//		}
-//		if(b<a) {
-//			if(a<c) {
-////				b<a<c
-//				d = a;
-//				a = b;
-//				b = d;
-//			}else if(b<c) {
-////				b<c<a
-//				d = a;
-//				a = b;
-//				b = c;
-//				c = d;
-//			}
-//		}
-//		if(c<b) {
-//			if(b<a) {
-////				c<b<a
-//				d = a;
-//				a = c;
-//				c = d;
-//			}else if(c<a) {
-////				c<a<b
-//				d = a;
-//				a = c;
-//				c = b;
-//				b = d;
-//			}
-//		}
-//		System.out.println(String.format("a:%d, b:%d, c:%d", a,b,c));
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a : ");
+		int a = Integer.parseInt(sc.nextLine());
+		System.out.print("b : ");
+		int b = Integer.parseInt(sc.nextLine());
+		System.out.print("c : ");
+		int c = Integer.parseInt(sc.nextLine());
+		int d = 0;
+		
+		if(a<b) {
+			if(b<c) {
+//				a<b<c
+			}else if(a<c) {
+//				a<c<b
+				d = b;
+				b = c;
+				c = d;
+			}
+		}
+		if(b<a) {
+			if(a<c) {
+//				b<a<c
+				d = a;
+				a = b;
+				b = d;
+			}else if(b<c) {
+//				b<c<a
+				d = a;
+				a = b;
+				b = c;
+				c = d;
+			}
+		}
+		if(c<b) {
+			if(b<a) {
+//				c<b<a
+				d = a;
+				a = c;
+				c = d;
+			}else if(c<a) {
+//				c<a<b
+				d = a;
+				a = c;
+				c = b;
+				b = d;
+			}
+		}
+		System.out.println(String.format("a:%d, b:%d, c:%d", a,b,c));
 		
 	}
 }
---
+++ src/com/human/ex/Java0314_Ex03.java
@@ -0,0 +1,182 @@
+package com.human.ex;
+
+import java.util.Scanner;
+
+public class Java0314_Ex03 {
+	
+	public static void main(String[] args) {
+		
+		
+//		문제1)
+//		else if문
+//		Scanner sc = new Scanner(System.in);
+//		int input = Integer.parseInt(sc.nextLine());
+//		int a = 5;
+//		int b = 7;
+//		
+//		if(input==0) {
+//			a=a+2;
+//		}else if(input==1){
+//			b=b+4;
+//		}else if(input==2){
+//			a=a+b;
+//		}else {
+//			b=b+5;
+//		}
+//		System.out.println(String.format("a=%d, b=%d", a,b));
+//	}
+//		
+//		switch문
+//		Scanner sc = new Scanner(System.in);
+//		int input = Integer.parseInt(sc.nextLine());
+//		int a = 5;
+//		int b = 7;
+//		
+//		switch(input) {
+//			case 0 : 
+//				a=a+2;
+//				break;
+//			case 1 :
+//				b=b+4;
+//				break;
+//			case 2 :
+//				a=a+b;
+//				break;
+//			default :
+//				b=b+5;
+//				break;
+//		}
+//		System.out.println(String.format("a=%d, b=%d", a,b));
+		
+		
+		
+//		문제2)
+//		else if문
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("점수 입력 : ");
+//		int score = Integer.parseInt(sc.nextLine());
+//		
+//		if(score>=90) {
+//			System.out.println("수");
+//		}else if(score>=80) {
+//			System.out.println("우");
+//		}else if(score>=70) {
+//			System.out.println("미");
+//		}else if(score>=60) {
+//			System.out.println("양");
+//		}else {
+//			System.out.println("가");
+//		}
+//		
+//		switch문
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("점수 입력 : ");
+//		int score = Integer.parseInt(sc.nextLine());
+//		
+//		switch(score/10) {
+//			case 10 :
+//				System.out.println("수");
+//				break;
+//			case 9 :
+//				System.out.println("수");
+//				break;
+//			case 8 :
+//				System.out.println("우");
+//				break;
+//			case 7 :
+//				System.out.println("미");
+//				break;
+//			case 6 :
+//				System.out.println("양");
+//				break;
+//			default :
+//				System.out.println("가");
+//				break;
+//		}
+		
+		
+		
+//		문제3)
+//		else if문
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("1~5사이 숫자 입력 : ");
+//		int input = Integer.parseInt(sc.nextLine());
+//		
+//		if(input==1) {
+//			System.out.println("일");
+//		}else if(input==2) {
+//			System.out.println("이");
+//		}else if(input==3) {
+//			System.out.println("삼");
+//		}else if(input==4) {
+//			System.out.println("사");
+//		}else if(input==5) {
+//			System.out.println("오");
+//		}else {
+//			System.out.println("입력 오류");
+//		}
+//
+//		switch문
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("1~5사이 숫자 입력 : ");
+//		int input = Integer.parseInt(sc.nextLine());
+//
+//		switch (input) {
+//		case 1:
+//			System.out.println("일");
+//			break;
+//		case 2:
+//			System.out.println("이");
+//			break;
+//		case 3:
+//			System.out.println("삼");
+//			break;
+//		case 4:
+//			System.out.println("사");
+//			break;
+//		case 5:
+//			System.out.println("오");
+//			break;
+//		default:
+//			break;
+//		}
+	
+		
+		
+//		문제4)
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("몇 월? : ");
+//		int month = Integer.parseInt(sc.nextLine());
+//		
+//		switch (month) {
+//		case 1, 3, 5, 7, 8, 10, 12 :
+//			System.out.println(month + "월은 31일 입니다.");
+//			break;
+//		case 2 :
+//			System.out.println(month + "월은 28일(29일) 입니다.");
+//		break;
+//		case 4, 6, 9, 11 :
+//			System.out.println(month + "월은 30일 입니다.");
+//		break;
+//		}
+		
+	
+//		문제5)
+		Scanner sc = new Scanner(System.in);
+		System.out.print("나이는 : ");
+		int age = Integer.parseInt(sc.nextLine());
+		
+		if(age>19) {
+			System.err.println("잘못 입력");
+		}else if(age>=17) {
+			System.out.println("고등학교");
+		}else if(age>=14) {
+			System.out.println("중학교");
+		}else if(age>=11) {
+			System.out.println("초등학교");
+		}else {
+			System.out.println("잘못 입력");
+		}
+	}
+
+}

c5063c5604173f95a223f84da57c11477f62cbc9
kyb <kyb@218.38.137.27>
2022-03-15 11:50:57 +0900

    2 commit

--- src/com/human/ex/Java0315_Ex01.java
+++ src/com/human/ex/Java0315_Ex01.java
@@ -1,5 +1,6 @@
 package com.human.ex;
 
+import java.util.Date;
 import java.util.Scanner;
 
 public class Java0315_Ex01 {
@@ -40,7 +41,7 @@ public class Java0315_Ex01 {
 //		b = a>12;
 //		c = a<=22;
 //		
-//		if(!b==true||!c==true) {
+//		if(!(b==true&&c==true)) {
 //			System.out.println(true);
 //		}else {
 //			System.out.println(false);
@@ -113,25 +114,119 @@ public class Java0315_Ex01 {
 //			System.out.println("버스 요금은 1500원 입니다.");
 //		}else if(age>15==true&&!a==true) {
 //			System.out.println("버스 요금은 1600원 입니다.");
+//		}else {
+//			System.out.println("오류");
 //		}
 		
 		
 //		문제5)
-		Scanner sc = new Scanner(System.in);
-		System.out.print("무엇을 먹었는가? (밥/빵) : ");
-		String input = sc.nextLine();
-		System.out.println("다른 걸 먹었는가? (true/false) : ");
-		boolean a = Boolean.parseBoolean(sc.nextLine());
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("무엇을 먹었는가? (밥/빵) : ");
+//		String input = sc.nextLine();
+//		System.out.println("다른 걸 먹었는가? (true/false) : ");
+//		boolean a = Boolean.parseBoolean(sc.nextLine());
+//		
+//		if(input.equals("밥")&&a==true) {
+//			System.out.println("밥을 먹었음");
+//		}else if(input.equals("밥")&&!a==false) {
+//			System.out.println();
+//		}else if(input.equals("빵")&&a==true) {
+//			System.out.println("빵을 먹었음");
+//		}else if(input.equals("빵")&&!a==true) {
+//			System.out.println("밥을 먹었음");
+//		}
 		
-		if(input.equals("밥")&&a==true) {
-			System.out.println("밥을 먹었음");
-		}else if(input.equals("밥")&&!a==false) {
-			System.out.println();
-		}else if(input.equals("빵")&&a==true) {
-			System.out.println("빵을 먹었음");
-		}else if(input.equals("빵")&&!a==true) {
-			System.out.println("밥을 먹었음");
-		}
+		
+//		문제6)
+//		int i = new Date().getDay();
+//		
+//		switch (i) {
+//		case 1:
+//			System.out.println("월요일");
+//			break;
+//		case 2:
+//			System.out.println("화요일");
+//			break;
+//		case 3:
+//			System.out.println("수요일");
+//			break;
+//		case 4:
+//			System.out.println("목요일");
+//			break;
+//		case 5:
+//			System.out.println("금요일");
+//			break;
+//		case 6:
+//			System.out.println("토요일");
+//			break;
+//		case 7:
+//			System.out.println("일요일");
+//			break;
+//		}
+		
+		
+//		문제7)
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("한국 SIZE 입력 : ");
+//		int kor = Integer.parseInt(sc.nextLine());
+//		boolean s = kor>=90&&kor<95;
+//		boolean m = kor>=95&&kor<100;
+//		boolean l = kor>=100&&kor<105;
+//		boolean xl = kor>=105&&kor<110;
+//		boolean xxl = kor>=110;
+//		
+//		if(xxl) {
+//			System.out.println("미국 SIZE : XXL");
+//		}else if(xl) {
+//			System.out.println("미국 SIZE : XL");
+//		}else if(l) {
+//			System.out.println("미국 SIZE : L");
+//		}else if(m) {
+//			System.out.println("미국 SIZE : M");
+//		}else if(s) {
+//			System.out.println("미국 SIZE : S");
+//		}else {
+//			System.out.println("SIZE 잘못 입력");
+//		}
+		
+//		if(kor>=110) {
+//			System.out.println("미국 SIZE : XXL");
+//		}else if(kor>=105) {
+//			System.out.println("미국 SIZE : XL");
+//		}else if(kor>=100) {
+//			System.out.println("미국 SIZE : L");
+//		}else if(kor>=95) {
+//			System.out.println("미국 SIZE : M");
+//		}else if(kor>=90) {
+//			System.out.println("미국 SIZE : S");
+//		}
+		
+		
+		
+//		문제8)
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("java 점수 : ");
+//		int jScore = Integer.parseInt(sc.nextLine());
+//		System.out.print("html 점수 : ");
+//		int hScore = Integer.parseInt(sc.nextLine());
+//		System.out.print("db 점수 : ");
+//		int dScore = Integer.parseInt(sc.nextLine());
+//		int avg = (jScore+hScore+dScore)/3;
+//		
+//		if(avg>=60) {
+//			if(jScore<=40) {
+//				System.out.print(String.format("java %d 과락", jScore));
+//			}else if(hScore<=40) {
+//				System.out.print(String.format("html %d 과락", hScore));
+//			}else if(dScore<=40) {
+//				System.out.print(String.format("db %d 과락", dScore));
+//			}else {
+//				System.out.println("합격");
+//			}
+//		}else {
+//			System.out.println("불합격");
+//		}
+		
 		
 		
 		

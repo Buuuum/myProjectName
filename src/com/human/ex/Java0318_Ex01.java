05e2ffb4328c7cb5f31f62eb1649397b9bfb1116
kyb <kyb@218.38.137.27>
2022-03-17 17:24:06 +0900

    2 commit

---
+++ src/com/human/ex/Java0318_Ex01.java
@@ -0,0 +1,82 @@
+package com.human.ex;
+
+public class Java0318_Ex01 {
+
+	
+	public static void main(String[] args) {
+		
+		
+//		문제1)
+//		int[] a = {1, 2, 3};
+//		
+//		for(int i=0; i<3; i++) {
+//			a[i] = a[i]+2;
+//			System.out.println(a[i]);
+//		}
+		
+		
+		
+//		문제2)
+//		int[] a = new int[10];
+//		
+//		for(int i=0; i<10; i++) {
+//			a[i] = i*3+3;
+//			System.out.println(a[i]);
+//		}
+		
+		
+		
+//		문제3)
+//		int[] a = new int[100];
+//		
+//		for(int i=0; i<100; i++) {
+//			a[i] = i+1;
+//			if(i>0&&i%2==0) {
+//				System.out.println(a[i]);
+//			}
+//		}
+		
+		
+		
+//		문제4)
+//		int[] a = {12,1,5,3,6,8,5,3};
+//		int sum = 0;
+//		
+//		for(int i=0; i<8; i++) {
+//			sum += a[i];
+//		}
+//		System.out.println(sum);
+		
+		
+		
+//		문제5)
+//		int[] a = {12,1,5,3,6,8,5,3};
+//		int sum = 0;
+//		
+//		for(int i=0; i<8; i++) {
+//			if(a[i]%2==0) {
+//				sum += a[i];
+//			}
+//		}
+//		System.out.println(sum);
+		
+		
+		
+//		문제6)
+//		int[] a = {12,1,51,3,6,8,5};
+//		int max = a[0];
+//		int min = a[0];
+//		
+//		for(int i=0; i<7; i++) {
+//			if(min > a[i]){
+//                min = a[i];
+//            }
+//            if(max < a[i]) {
+//                max = a[i];
+//            }
+//		}
+//		System.out.println(max+min);
+		
+		
+	}
+}

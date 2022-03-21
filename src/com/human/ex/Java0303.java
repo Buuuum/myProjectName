602976b6de141ee4af4aa07190317fa5996f85b2
kyb <kyb@218.38.137.27>
2022-03-21 17:23:54 +0900

    4 commit

--- src/com/human/ex/Java0303.java
+++ src/com/human/ex/Java0303.java
@@ -29,6 +29,17 @@ public class Java0303 {
 //			}
 //			System.out.println();
 //		}
+		
+		
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<6-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=1; k<i*2; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 
 
 //		System.out.println("*****************");

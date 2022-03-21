183c9f48fbc3b34b1edf09aae988b60335ffd258
kyb <kyb@218.38.137.27>
2022-03-21 16:55:54 +0900

    3 commit

--- src/com/human/ex/Java0321.java
+++ src/com/human/ex/Java0321.java
@@ -12,41 +12,8 @@ public class Java0321 {
 		//			}
 		//		}
 		//		System.out.println(sum);
-
-
-
-
-
-		for(int i =1; i<100; i++) {
-
-			int a=i/10;
-
-			int b=i%10;
-
-			if(a>0 && b>0 && a%3==0 && b%3==0) {
-
-				System.out.println(i+"짝짝");
-
-			}
-
-			else if(a>0 &&a%3==0) {
-
-				System.out.println(i+"짝");
-
-			}
-
-			else if(b>0 &&b%3==0) {
-
-				System.out.println(i+"짝");
-
-			}
-
-			else {
-
-				System.out.println(i);
-
-			}
-
-		}
+		
+		
+		
 	}
 }

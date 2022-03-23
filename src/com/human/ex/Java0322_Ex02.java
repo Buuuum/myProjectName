4d4645c08fc59606023e43750a45e3a493bc4224
kyb <kyb@218.38.137.27>
2022-03-23 10:05:51 +0900

    1 commit

--- src/com/human/ex/Java0322_Ex02.java
+++ src/com/human/ex/Java0322_Ex02.java
@@ -312,61 +312,57 @@ public class Java0322_Ex02 {
 //			}
 		
 //		Q19)
-//		for (int i=1; i<=9; i++) {
-//			String stars = "";
-//			int spaceWidth = 0;
-//			int starWidth = 0;
-//			if (i<5) {
-//				int upperI = i-1;
-//				starWidth = 5-upperI;
-//				// 공백 1, 3, 5...
-//				spaceWidth = 2*upperI;
-//			} else {
-//				int lowerI = i-4;
-//				starWidth = lowerI;
-//				spaceWidth = 10-2*lowerI;
+//		for(int i=0; i<5; i++) {
+//			for(int j=0; j<5-i; j++) {
+//				System.out.print("*");
 //			}
-//			// 왼쪽 별
-//			for (int j=1; j<=starWidth; j++) {
-//				stars += "*";
+//			for(int j=0; j<(i*2)+1; j++) {
+//				System.out.print(" ");
 //			}
-//			// 공백
-//			for (int j=1;j<=spaceWidth+1; j++) {
-//				stars += " ";
+//			for(int j=0; j<5-i; j++) {
+//				System.out.print("*");
 //			}
-//			// 오른쪽 별
-//			for (int j=1; j<=starWidth; j++) {
-//				stars += "*";
+//			System.out.println();
+//		}
+//		for(int i=1; i<5; i++) {
+//			for(int j=0; j<i+1; j++) {
+//				System.out.print("*");
 //			}
-//			System.out.println(stars);
+//			for(int j=0; j<9-(i*2); j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<i+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
 //		}
 		
 		
 		
-		
 //		Q20)
-//		int i,j;
-//		int sign=1;
-//		for(i=1;i>0;i+=sign) {
-//			for(j=0;j<5;j++) {
-//				if(i>j) {
-//					System.out.print("*");
-//				}else {
-//					System.out.print(" ");
-//				}	
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
 //			}
-//			System.out.print(" ");
-//			for(j=0;j<5;j++) {
-//				if(j<=4-i) {
-//					System.out.print(" ");
-//				}else {
-//					System.out.print("*");
-//				}				
+//			for(int j=1; j<12-(i*2); j++) {
+//				System.out.print(" ");
 //			}
-//			System.out.println("");
-//			if(i==5) {
-//				sign=-1;
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
 //			}
+//			System.out.println();
+//		}
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<6-i; j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=0; j<(i*2)+1; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=1; j<6-i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
 //		}
 
 		
---
+++ src/com/human/ex/Java0323.java
@@ -0,0 +1,54 @@
+package com.human.ex;
+
+public class Java0323 {
+	
+	
+	public static void main(String[] args) {
+		
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<2-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=0; j<i+1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=1; j<12-(i*2); j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		for(int i=1; i<6; i++) {
+//			for(int j=1; j<6-i; j++) {
+//				System.out.print("*");
+//			}
+//			for(int j=0; j<(i*2)+1; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=1; j<6-i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}

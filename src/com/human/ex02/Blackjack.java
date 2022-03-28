a73fa9d69d072a0b581c8e2633412b2600e67fd6
admin <admin@DESKTOP-APDSTRB>
2022-03-28 15:50:52 +0900

    3 commit

--- src/com/human/ex02/Blackjack.java
+++ src/com/human/ex02/Blackjack.java
@@ -1,12 +1,11 @@
 package com.human.ex02;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Blackjack {
 
-	
-	
+
+
 	public static void main(String[] args) {
 
 		String[] cardshape = {"하트","다이아","스페이드","클로버"};
@@ -37,14 +36,16 @@ public class Blackjack {
 		int p2DeckIndex = 0;
 
 
-//		p1Deck[p1DeckIndex++] = deck[deckIndex++];
-//		p2Deck[p2DeckIndex++] = deck[deckIndex++];
-//
-		
+		//		p1Deck[p1DeckIndex++] = deck[deckIndex++];
+		//		p2Deck[p2DeckIndex++] = deck[deckIndex++];
+		//
+
 		Scanner sc = new Scanner(System.in);
 		boolean isP1PlayFlag = true;
 		boolean isP2PlayFlag = true;
-	
+		int p1Score = 0;
+		int p2Score = 0;
+		
 		while((isP1PlayFlag||isP2PlayFlag)&&p1DeckIndex<p1Deck.length&&p2DeckIndex<p2Deck.length) {
 			if(isP1PlayFlag) {
 				System.out.print("\np1 카드를 받으시겠습니까?(1.yes 2.no) : ");
@@ -53,7 +54,26 @@ public class Blackjack {
 					p1Deck[p1DeckIndex++] = deck[deckIndex++];
 					System.out.println("p1");
 					for(int i=0; i<p1DeckIndex; i++) {
+						p1Score = 0;
 						System.out.println("Deck["+i+"] : " + cardshape[p1Deck[i]/13]+" "+cardNumber[p1Deck[i]%13]);
+					}
+					for(int i=0;i<p1DeckIndex;i++) {
+						if(p1Deck[i]%13==0) {
+							p1Score+=11;
+						}else if(p1Deck[i]%13<10) {
+							p1Score+=p1Deck[i]%13+1;
+						}else {
+							p1Score+=10;
+						} 
+					}
+					for(int i=0;p1Score>21;i++) {
+						if(p1Deck[i]==0) {
+							p1Score=p1Score-10;
+						} 
+					}
+					System.out.println("p1 점수 : " + p1Score);
+					if(p1Score>21) {
+						break;	
 					}
 				}else {
 					System.out.println("p1 카드 받기 종료\n");
@@ -67,59 +87,66 @@ public class Blackjack {
 					p2Deck[p2DeckIndex++] = deck[deckIndex++];
 					System.out.println("p2");
 					for(int i=0; i<p2DeckIndex; i++) {
+						p2Score = 0;
 						System.out.println("Deck["+i+"] : " + cardshape[p2Deck[i]/13]+" "+cardNumber[p2Deck[i]%13]);
 					}
+					for(int i=0;i<p2DeckIndex;i++) {
+						if(p2Deck[i]%13==0) {
+							p2Score+=11;
+						}else if(p2Deck[i]%13<10) {
+							p2Score+=p2Deck[i]%13+1;
+						}else {
+							p2Score+=10;
+						} 
+					}
+					for(int i=0;p2Score>21;i++) {
+						if(p2Deck[i]==0) {
+							p2Score=p2Score-10;
+						} 
+					}
+					if(p2Score>21) {
+						break;	
+					}
+					System.out.println("p2 점수 : " + p2Score);
 				}else {
 					System.out.println("p2 카드 받기 종료\n");
 					isP2PlayFlag = false;
 				}
 			}
+			
+			
+
 			if(!isP1PlayFlag&&!isP2PlayFlag) {
 				System.out.println("p1");
 				for(int i=0; i<p1DeckIndex; i++) {
 					System.out.println("Deck["+i+"] : " + cardshape[p1Deck[i]/13]+" "+cardNumber[p1Deck[i]%13]);
 				}
-
+				
 				System.out.println("p2");
 				for(int i=0; i<p2DeckIndex; i++) {
 					System.out.println("Deck["+i+"] : " + cardshape[p2Deck[i]/13]+" "+cardNumber[p2Deck[i]%13]);
 				}
+				System.out.println("p1 점수 : " + p1Score);
+				System.out.println("p2 점수 : " + p2Score);
+				if(p1Score>21) {
+					System.out.println("p1이 이겼습니다");
+				}else if(p2Score>21) {
+					System.out.println("p2가 이겼습니다.");
+				}else if(p1Score>p2Score) {
+					System.out.println("p1이 이겼습니다");
+				}else if(p1Score<p2Score) {
+					System.out.println("p2가 이겼습니다");
+				}else {
+					System.out.println("무승부");
+				}
 			}
-		}
-		
-		
-		
-		
-		
 
-		
-		
-		
+		}
+
+
+
+
 	}
-	
-	
-//	static void p1Score() {
-//		for(int i=0;i<p1DeckIndex;i++) {
-//			if(p1Deck[i]%13==0) {
-//				p1Scoure+=11;//일단 A를 11으로 놓고 계산함
-//			}else if(p1Deck[i]%13<10) {//A~10
-//				p1Scoure+=p1Deck[i]%13+1;
-//			}else {
-//				p1Scoure+=10;
-//			} 
-//		}
-//	}
-//	
-//	static void p2Score() {
-//		for(int i=0;i<p2DeckIndex;i++) {
-//			if(p2Deck[i]%13==0) {
-//				p2Scoure+=11;//일단 A를 11으로 놓고 계산함
-//			}else if(p2Deck[i]%13<10) {//A~10
-//				p2Scoure+=p2Deck[i]%13+1;
-//			}else {
-//				p2Scoure+=10;
-//			} 
-//		}
-//	}
-	
+
 }
+

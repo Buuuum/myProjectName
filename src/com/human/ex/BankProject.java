b1828fa735aa67e5b461fd284b29717102e16ba4
kyb <kyb@218.38.137.27>
2022-03-30 15:24:49 +0900

    2 commit

--- src/com/human/ex/BankProject.java
+++ src/com/human/ex/BankProject.java
@@ -5,25 +5,17 @@ import java.util.Scanner;
 
 
 class User{
-
+	
 	public static final int LOGIN_LOG_OFF = 0;
 	public static final int LOGIN_NOMAL_USER = 1;
 	public static final int LOGIN_ADMIN_USER = 2;
 
-
-	public static int loginState = LOGIN_LOG_OFF;
-	public static String id = "admin";
-	public static String pw = "admin";
-	public static int account = 0;
-
+	public int loginState = LOGIN_LOG_OFF;
+	public String id = "";
+	public String pw = "";
+	public int account = 0;
 
 	public User() {	}
-	public User(int loginState, String id, String pw) {
-		super();
-		this.loginState = loginState;
-		this.id = id;
-		this.pw = pw;
-	}
 	public User(int loginState, String id, String pw, int account) {
 		super();
 		this.loginState = loginState;
@@ -31,6 +23,7 @@ class User{
 		this.pw = pw;
 		this.account = account;
 	}
+	
 
 	@Override
 	public String toString() {
@@ -44,17 +37,26 @@ class User{
 
 
 public class BankProject {
+	
+
 
 	public static void main(String[] args) {
-
-		User[] userArray= new User[5];
+		
+		
+		
+		User[] userArray= new User[10];
 		int userCount = 0;
 		
-		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER, "user1", "user1", 0);
-		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER, "user2", "user2", 0);
-		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER, "user3", "user3", 0);
-		userArray[userCount++] = new User(User.LOGIN_ADMIN_USER, "admin1", "admin1");
-		userArray[userCount++] = new User(User.LOGIN_ADMIN_USER, "admin2", "admin2");
+		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER,"user1","user1",0);
+		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER,"user2","user2",0);
+		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER,"user3","user3",0);
+		userArray[userCount++] = new User(User.LOGIN_NOMAL_USER,"user4","user4",0);
+		userArray[userCount++] = new User(User.LOGIN_ADMIN_USER,"admin1","admin1",0);
+		userArray[userCount++] = new User(User.LOGIN_ADMIN_USER,"admin2","admin2",0);
+		
+		for(User i : userArray) {
+			System.out.println(i);
+		}
 
 		
 		String inputId = "";
@@ -70,34 +72,121 @@ public class BankProject {
 			inputPw = new Scanner(System.in).nextLine();
 			
 			
-			User nowUser = userFind(inputId,userArray);
-
-
-
+//			User nowUser = userFind(inputId,userArray);
+			User nowUser = null;
+			for(int i=0; i<userCount; i++) {
+				if(nowUser.id.equals(inputId)&&nowUser.pw.equals(inputPw)) {
+					nowUser = userArray[i];
+				}
+			}
 			if(nowUser!=null) {
 				switch(nowUser.loginState) {
 				case User.LOGIN_LOG_OFF:
-					System.out.println("사용할 수 없는 ID입니다.");
+					System.out.println("로그아웃 상태");
 					break;
 				case User.LOGIN_NOMAL_USER:
-					System.out.println(User.id+"님 로그인 하셨습니다.");
-					System.out.println("1.입금 2.출금 3.확인 4.종료 입력 >>");
+					System.out.println(inputId+"님 로그인 하셨습니다.");
+					boolean isNomalFlag = true;
+					while(isNomalFlag) {
+						System.out.println("1.입금 2.출금 3.잔액확인 4.로그아웃 입력 >>");
+						int menu = Integer.parseInt(new Scanner(System.in).nextLine());
+						switch (menu) {
+						case 1: 
+							System.out.println("얼마를 입금하시겠습니까? : ");
+							int deposit = Integer.parseInt(new Scanner(System.in).nextLine());
+							nowUser.account += deposit;
+							System.out.println("잔액 : "+nowUser.account);
+							break;
+						case 2: 
+							System.out.println("얼마를 출금하시겠습니까? : ");
+							int withdraw = Integer.parseInt(new Scanner(System.in).nextLine());
+							if(nowUser.account>withdraw) {
+								nowUser.account -= withdraw;
+								System.out.println("잔액 : "+nowUser.account);
+							}else {
+								System.out.println("잔액이 부족합니다.");
+							}
+							break;
+						case 3: 
+							System.out.println("잔액 : "+nowUser.account);
+							break;
+						case 4: 
+							System.out.println("로그아웃 되었습니다.");
+							isNomalFlag = false;
+							break;
+						default:
+							System.out.println("잘못 입력하셨습니다.");
+						}
+					}
 					break;
 				case User.LOGIN_ADMIN_USER:
-					System.out.println(User.id+"님 로그인 하셨습니다.");
-					System.out.println("1.계정추가 2.계정삭제 3.id변경 4.모든 사용자 출력 6.종료 입력 >>");
+					System.out.println(inputId+"님 로그인 하셨습니다.");
+					boolean isAdminFlag = true;
+					while(isAdminFlag) {
+						System.out.println("1.ID추가 2.ID삭제 3.id변경 4.모든 사용자 출력 0.로그아웃 입력 >>");
+						int menu = Integer.parseInt(new Scanner(System.in).nextLine());
+						switch (menu) {
+						case 1: 
+							System.out.println("추가할 ID 입력 >>");
+							String fId = new Scanner(System.in).nextLine();
+							System.out.println("PW 입력 >>");
+							String fPw = new Scanner(System.in).nextLine();
+							userArray[userCount++] = new User(User.LOGIN_ADMIN_USER, fId, fPw, 0);
+							System.out.println("ID가 추가되었습니다.");
+							break;
+						case 2: 
+							User findUser = null;
+							System.out.println("ID 입력 >>");
+							fId = new Scanner(System.in).nextLine();
+							System.out.println("PW 입력 >>");
+							fPw = new Scanner(System.in).nextLine();
+							for(int i=0; i<userCount; i++) {
+								if(userArray[i].id.equals(fId)&&userArray[i].pw.equals(fPw)) {
+									findUser = userArray[i];
+									for(int j=i; j<userCount-1; j++) {
+										userArray[j] = userArray[j+1];
+									}
+									userCount--;
+									break;
+								}
+							}
+							break;
+						case 3: 
+							System.out.println("변경	전 ID 입력 >>");
+							fId = new Scanner(System.in).nextLine();
+							System.out.println("변경 후 ID 입력 >>");
+							String newId = new Scanner(System.in).nextLine();
+							System.out.println("PW 입력 >>");
+							fPw = new Scanner(System.in).nextLine();
+							
+							for(int i=0; i<userCount; i++) {
+								if(userArray[i].id.equals(fId)&&userArray[i].pw.equals(fPw)) {
+									userArray[i].id = newId;
+								}
+							}
+							System.out.println("ID가 변경되었습니다.");
+							break;
+						case 4: 
+							for(int i=0; i<userCount; i++) {
+								System.out.println("ID : " + userArray[i].id);
+							}
+							break;
+						case 0: 
+							System.out.println("로그아웃 되었습니다.");
+							isAdminFlag = false;
+							break;
+						default:
+							System.out.println("잘못 입력하셨습니다.");
+						}
+					}
 					break;
 				}
 
 			}else {
-				
+				System.out.println("잘못 입력하셨습니다.");
 			}
 		}
 	}
 
-	private static User userFind(String inputId, User[] userArray) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
